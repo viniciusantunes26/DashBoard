c181f505eecdf50428f5f8e0c9a917e45481e723
lucasdcsilva04 <jeffersond1silva@hotmail.com>
2023-11-28 20:56:53 -0300

    update archives

--- dashboard/app/(tabs)/relatory.tsx
+++ dashboard/app/(tabs)/relatory.tsx
@@ -1,7 +1,7 @@
 import { StyleSheet, View } from "react-native";
 
-export default function TabOneScreen() {
-  return <View></View>;
+export default function TabTwoScreen() {
+  return <View style={styles.container}></View>;
 }
 
 const styles = StyleSheet.create({
