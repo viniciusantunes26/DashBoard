bf2c0b71d353f1a5a0b97678d0e3c0469f88a0b6
lucasdcsilva04 <jeffersond1silva@hotmail.com>
2023-11-28 20:59:16 -0300

    top

--- dashboard/app/(tabs)/index.tsx
+++ dashboard/app/(tabs)/index.tsx
@@ -4,19 +4,4 @@ export default function TabOneScreen() {
   return <View></View>;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
-  },
-});
+const styles = StyleSheet.create({});
--- dashboard/app/(tabs)/user.tsx
+++ dashboard/app/(tabs)/user.tsx
@@ -1,22 +1,7 @@
 import { StyleSheet, View } from "react-native";
 
 export default function TabOneScreen() {
-  return <View style={styles.container}></View>;
+  return <View></View>;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
-  },
-});
+const styles = StyleSheet.create({});
